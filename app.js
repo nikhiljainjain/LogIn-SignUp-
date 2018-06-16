fe876ba86a8bc42f652b078e6bfedfcbf26e6174
nikhiljainjain <34137170+nikhiljainjain@users.noreply.github.com>
2018-06-16 19:58:30 +0530

    Update app.js
    File with working basic signUp and Login system. Updated on 16 June 2018

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require('express'),
     app = express(),
+    nodemailer = require('nodemailer'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cookieParser = require('cookie-parser'),
     morgan = require('morgan'),
     jwt = require('jsonwebtoken'),
     config = require('./config'),
-    User = require('./app/models/user');
+    Employee = require('./app/models/employee'),
+    Log = require('./app/models/log'),
+    Admin = require('./app/models/admin');
 
 app.listen(80, ()=>{
     console.log('Server started');
 })
 
 app.use(morgan('dev'));
-
+/*
+var transporter = nodemailer.createTransport({
+    service : 'gmail',
+    auth : {
+        user : 'myemailid',
+        pass : 'password'
+    }
+})
+*/
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -30,65 +39,154 @@ mongoose.connect(config.database, (err)=>{
 
 app.set('superSecret', config.secret);
 
-app.use(cookieParser());
-
 app.get('/', (req, res)=>{
     res.render('index.ejs');
 })
 
 app.post('/login', (req, res)=>{
-    console.log(req.body);
-    res.send('you are login');
+    Employee.findOne({emailId: req.body.emailId}, (err, user)=>{
+        if (err) throw err;
+        if (!user){
+            Admin.findOne({emailId: req.body.emailId}, (err, userAdmin)=>{
+                  if (err) throw err;
+                  if (!userAdmin){
+                      res.send('Email not found');
+                  }else if (userAdmin){
+                      if (userAdmin.password != req.body.password){
+                          res.send('Password not match');
+                      }
+                  }else {
+                      const payload = {
+                          userName : userAdmin.userName,
+                          department : userAdmin.department
+                      };
+                      var token = jwt.sign(payload, app.get('superSecret'), {
+                          expiresIn : '1h'
+                      });
+                      res.render('adminLogin.ejs', {employees: userAdmin.employees, userName: userAdmin.userName,
+                          department: userAdmin.department, token: token});
+                      //token = payload = undefined;
+                  }
+            })
+        }else if (user){
+            if (user.password != req.body.password){
+                res.send('Authecation failed');
+            }else {
+                if (user.emailConfirmation) {
+                    const payload = {
+                        userName : user.userName
+                    };
+                    var token = jwt.sign(payload, app.get('superSecret'), {
+                        expiresIn: '1h'
+                    });
+                    res.render('employeeLogin.ejs', {name: user.userName, work: user.work, token: token});
+                    //token = payload = undefined;
+                }
+                else {
+                    res.send('Your email is not confirme please check your email box');
+                }
+            }
+        }
+    })
 })
 
 app.post('/newUser', (req, res)=>{
-    console.log(req.body);
-    res.end('nikhiljain');
-})
-
-/*var logSchema = new mongoose.Schema({
-    work:   Array({
-        work: String,
-        assignBy: String
+    Employee.find({password: req.body.password}, (err, data)=>{
+        if (err){
+            console.log(err);
+            throw err;
+        }
+        if (data.length !== 0) {
+            res.send('You are not allow for this password');
+        }else{
+            Admin.find({password : req.body.password}, (err, data)=> {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+                if (data.length !== 0) {
+                    res.send('This password is not allowed');
+                }else{
+                    Employee.find({emailId : req.body.emailId}, (err, data)=>{
+                    if (err) {
+                        console.log(err);
+                        throw err;
+                    }if (data.length !== 0){
+                        res.send('This is email already register');
+                    }else {
+                        var employe = {userName: req.body.userName, password: req.body.password, emailId: req.body.emailId, hashCode : (Math.random())*100000000000000};
+                        Employee.create(employe, (err, data)=>{
+                            if (err) {
+                                console.log(err);
+                                throw err;
+                            }
+                            console.log(data);
+                            res.send('<h1>You are successfully register !!!</h1><br> Check your email box for confirmation');
+                            /*var mailOptions = {
+                                from : 'myemailId',
+                                to : req.body.emailId,
+                                subject : 'Email authencation',
+                                html : "<p>Please click below link for authentication before going to start with us</p>" +
+                                "<a href=/user/" + data._id/data.hashCode + "/emailConfimation>Click here </a>"
+                            }
+                            transporter.sendMail(mailOptions, (error, info)=>{
+                                if (error){
+                                    console.log(error);
+                                    throw error;
+                                }else{
+                                    console.log('Email send successfully '+ info.reponse);
+                                }
+                            })*/
+                            //mailOptions = undefined;
+                        })
+                        //employe = undefined;
+                        }
+                    })
+                }
+            })
+        }
     })
-});
-
-var log = mongoose.model('logs', logSchema);
-
-var  adminSchema = new mongoose.Schema({
-    userName : String,
-    password : String,
-    department : String,
-    emailId : String,
-    employees : Array({
-        name: String,
-        work: String,
-        email: String,
-        workStatus: Boolean,
-        deadLine: Date
-    } )
 })
 
-var admin = mongoose.model('admins', adminSchema);
-
-var employeeSchema = new mongoose.Schema({
-    userName : String,
-    password : String,
-    emailId : String,
-    forgotPassword : {
-        type: Number,
-        default: 0
-    },
-    work : Array({
-        title: String,
-        givenBy: String,
-        status: Boolean,
-        deadLine: Date
-    }),
-    authentication : Boolean,
-    emailConfirmation : Boolean,
-    hashCode : String
+app.get('/logs', (req, res)=>{
+    Log.find({}, (err, user)=>{s
+        if (err){
+            console.log(err);
+            throw err;
+        }
+        res.send(user);
+    })
 })
 
-var employee = mongoose.model('employees', employeeSchema);*/
-
+app.get('/user/:id/:hashCode/emailConfimation', (req, res)=>{
+    Employee.find({_id: req.params.id, hashCode: req.params.hashCode}, (err, data)=>{
+        if (err){
+            console.log(err);
+            throw err;
+        }if (data.length == 0){
+            res.send('You are not authorized please check link again');
+        }else{
+            Employee.findByIdAndUpdate(req.params._id, {emailConfirmation: true}, (err)=>{
+                if (err){
+                    throw err;
+                    console.log(err);
+                }
+            })
+            res.redirect('/');
+            /*var mailOptions = {
+                from : 'myemailId',
+                to : 'adminEmailId',
+                subject : 'User authencation',
+                text : 'Email is confirmed for the user '+ data.userName + ' please check for authencation.'
+            }
+            transporter.sendMail(mailOptions, (error, info)=>{
+                if (error){
+                    console.log(error);
+                    throw error;
+                }else{
+                    console.log('Email send successfully '+ info.reponse);
+                }
+            })*/
+        }
+    })
+})
