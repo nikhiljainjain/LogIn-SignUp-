8db7b248da2b364f57c2f921bb83efaea3ce5848
nikhiljainjain <34137170+nikhiljainjain@users.noreply.github.com>
2018-06-16 19:35:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret' : 'hackersneverdie' ,
-    'database' : 'mongodb://127.0.0.1:27017/users'
+    'secret' : 'Enterhere' ,
+    'database' : 'mongodb://127.0.0.1:27017/users' //enter your database address here
 }
